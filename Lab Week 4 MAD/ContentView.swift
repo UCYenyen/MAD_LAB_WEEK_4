5b9f127646264c34c237f07d51f1911a5a6d75a8
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-03-05 15:54:03 +0700

    Initial Commit

---
+++ Lab Week 4 MAD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab Week 4 MAD
+//
+//  Created by Bryan Fernando Dinata on 05/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
